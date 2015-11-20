9daad79eb29da3b8a9d1f9f6a65bf3274a658d7d
Cedric-Paris <Cedric.Paris@etu.udamail.fr>
2015-11-20 11:41:24 +0100

    Add basic Player
    With horizontal movement and square detector

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player : MonoBehaviour {
+
+    private float speed = -0.05f;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.Translate(new Vector2(speed, 0));
+	}
+}
