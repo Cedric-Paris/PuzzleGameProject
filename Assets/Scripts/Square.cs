e944fef91fdf2dc71a5dc4ff3e91f8f6cbf200ff
BMosnier <bastienmosnier@gmail.com>
2015-11-23 10:45:53 +0100

    Squares and Tiles
    Create prefabs Square and Tile

---
+++ Assets/Scripts/Square.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Square : MonoBehaviour {
+
+	public Element squareElement;
+
+	// Use this for initialization
+	void Start () {
+		if (squareElement!=null)
+			squareElement.transform.position = transform.position;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
