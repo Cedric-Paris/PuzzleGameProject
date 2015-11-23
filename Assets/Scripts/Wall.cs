29fe8be285ee3ec843cbbbb6c8f73797b2e75f73
Kwayoo <ribierel@yahoo.fr>
2015-11-23 10:27:19 +0100

    Script du wall

---
+++ Assets/Scripts/Wall.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Wall : Element {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+
+	
+	public override EffectTransformation Effect () {
+		EffectTransformation effect = new EffectTransformation ();
+		effect.isObstacle = true;
+		return effect;
+	}
+
+}
