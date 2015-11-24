05e7f96377490aebd69e744c673c82849a32b5f8
Cedric-Paris <Cedric.Paris@etu.udamail.fr>
2015-11-24 22:56:35 +0100

    Fix Player movement issues
    Correction du problème de saccade lors des changements de direction
    - Ajout d'un délégué qui oblige l'effet de case courant à s'activer
    quand le player est au centre de la case
    - Redéfinition des délégués en expression lambda (pour diminuer le code)

--- Assets/Scripts/DirectionProperties.cs
+++ Assets/Scripts/DirectionProperties.cs
@@ -6,18 +6,22 @@ public class DirectionProperties {
 	public delegate Vector3 CalculFavoritePos(Vector3 currentPos);
 	public CalculFavoritePos calculFavoritePos;
 
+	public delegate bool SquareCanBeTreat(Vector3 currentPos);
+	public SquareCanBeTreat squareCanBeTreat;
+
 	public Vector3 direction;
 
 	public Vector3 positionNextObserver;
 
 	public int animationCode;
 
-	public DirectionProperties (Vector3 direction, Vector3 positionNextObserver, int animationCode, CalculFavoritePos fonct)
+	public DirectionProperties (Vector3 direction, Vector3 positionNextObserver, int animationCode, CalculFavoritePos fonctCal, SquareCanBeTreat fonctTreat)
 	{
 		this.direction = direction;
 		this.positionNextObserver = positionNextObserver;
 		this.animationCode = animationCode;
-		this.calculFavoritePos = fonct;
+		this.calculFavoritePos = fonctCal;
+		this.squareCanBeTreat = fonctTreat;
 	}
 	
 }
--- Assets/Scripts/PlayerMovementController.cs
+++ Assets/Scripts/PlayerMovementController.cs
@@ -4,25 +4,33 @@ using System.Collections;
 public class PlayerMovementController : MonoBehaviour {
 
 	public static readonly DirectionProperties GO_UP = new DirectionProperties (new Vector3 (0, 1, 0), new Vector3 (0, 0.5f, 0), -1,
-	                                                                            delegate(Vector3 currentPos)
-	                                                                            {
-																					return new Vector3(currentPos.x, ((int)currentPos.y)+0.5f,0);
-																				});
+	                                                                            (currentPos) => { return new Vector3 (currentPos.x, ((int)currentPos.y) + 0.5f, 0);},
+																				(currentPos) => {
+																									if (currentPos.y < 0 && (currentPos.y * -1) % 1 <= 0.5f) return true;
+																									if (currentPos.y > 0 && (currentPos.y % 1) >= 0.5f) return true;
+																									return false;
+																								});
 	public static readonly DirectionProperties GO_DOWN = new DirectionProperties(new Vector3(0, -1, 0), new Vector3 (0, -0.5f, 0), -1,
-	                                                                             delegate(Vector3 currentPos)
-	                                                                             {
-		return new Vector3(currentPos.x, ((int)currentPos.y)+0.5f,0);
-	});
+	                                                                            (currentPos) => { return new Vector3 (currentPos.x, ((int)currentPos.y) + 0.5f, 0);},
+																				(currentPos) => {
+																									if (currentPos.y < 0 && (currentPos.y * -1) % 1 >= 0.5f) return true;
+																									if (currentPos.y > 0 && (currentPos.y % 1) <= 0.5f) return true;
+																									return false;
+																								});
 	public static readonly DirectionProperties GO_RIGHT = new DirectionProperties(new Vector3(1, 0, 0), new Vector3 (0.5f, 0, 0), -1,
-	                                                                              delegate(Vector3 currentPos)
-	                                                                              {
-		return new Vector3(((int)currentPos.x)+0.5f, currentPos.y,0);
-	});
+	                                                                              	(currentPos) => { return new Vector3(((int)currentPos.x)+0.5f, currentPos.y,0);},
+																					(currentPos) => {
+																										if (currentPos.x < 0 && (currentPos.x * -1) % 1 <= 0.5f) return true;
+																										if (currentPos.x > 0 && (currentPos.x % 1) >= 0.5f) return true;
+																										return false;
+																									});
 	public static readonly DirectionProperties GO_LEFT = new DirectionProperties(new Vector3(-1, 0, 0), new Vector3 (-0.5f, 0, 0), -1,
-	                                                                             delegate(Vector3 currentPos)
-	                                                                             {
-		return new Vector3(((int)currentPos.x)+0.5f, currentPos.y,0);
-																				 });
+                                                                            	(currentPos) => { return new Vector3(((int)currentPos.x)+0.5f, currentPos.y,0);},
+																				(currentPos) => {
+																									if (currentPos.x < 0 && (currentPos.x * -1) % 1 >= 0.5f) return true;
+																									if (currentPos.x > 0 && (currentPos.x % 1) <= 0.5f) return true;
+																									return false;
+																								});
 
 	public SquareObserver currentSquare;
 	public SquareObserver nextSquare;
@@ -37,7 +45,7 @@ public class PlayerMovementController : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		if ((!currentSquare.isTreated) && transform.position.x%1 >= 0.5f )
+		if ((!currentSquare.isTreated) && currentDirection.squareCanBeTreat(transform.position))
 			TreatSquare(currentSquare);
 		transform.Translate(currentDirection.direction * speed);
 	}
