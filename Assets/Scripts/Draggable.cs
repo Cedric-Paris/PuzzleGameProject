7fe608b084eeb1c31fa02f3735a19fc46d37ec59
Cedric-Paris <Cedric.Paris@etu.udamail.fr>
2015-11-28 16:11:30 +0100

    Add draggable elements
    - Ajout d'une classe abstraite Draggable (tout ce qui pourra être glissé
    - déposé héritera de cette classe)
    - Ajout d'une classe DraggableElement qui doit être associé à un élément
    pour le déplacer
    - L'élément se place au centre de la case la plus proche quand on le
    relâche
    - Tout objet doit avoir un collider pour être déplacé
    - Testé et fonctionnel sur portable avec Unity Remote

---
+++ Assets/Scripts/Draggable.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.EventSystems;
+
+public abstract class Draggable : MonoBehaviour {
+
+	//La methode OnMouseDrag necessite un collider sur l'element a bouger 
+	public void OnMouseDrag()
+	{
+		Vector3 mousePos = Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, 0));
+		mousePos.z = 0;
+		this.transform.position = mousePos; 
+	}
+
+	public void OnMouseUp()
+	{
+		Vector3 mousePos = Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, 0));
+		mousePos.z = 0;
+		this.transform.position = mousePos;
+		OnMouseDrop();
+
+	}
+
+	public abstract void OnMouseDrop();
+
+}
---
+++ Assets/Scripts/DraggableElement.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DraggableElement : Draggable {
+
+	public override void OnMouseDrop()
+	{
+		Vector3 favoritePosition = new Vector3 (0,0,0);
+		favoritePosition.x = CalculDemiLePlusProche (this.transform.position.x);
+		favoritePosition.y = CalculDemiLePlusProche (this.transform.position.y);
+
+		this.transform.position = favoritePosition;
+	}
+
+	private float CalculDemiLePlusProche(float value)
+	{
+		if (value < 0)
+			return ((int)value) - 0.5f;
+		return ((int)value) + 0.5f;
+	}
+}
