a760906b26d7d5bda2646fe678e9e10b99f6f1ec
Kwayoo <ribierel@yahoo.fr>
2015-11-30 09:58:47 +0100

    Final Case
    Changement de la hiérarchie de l'objectif
    Ajout d'un élément d'arrivé
    Ajout d'un élément objectif à ramasser (qui était au préalable dans la
    classe Objectif)

--- Assets/Scripts/EffectTransformation.cs
+++ Assets/Scripts/EffectTransformation.cs
@@ -18,4 +18,6 @@ public class EffectTransformation {
 
 	public bool isObjectif;
 
+	public bool isWinner;
+
 }
---
+++ Assets/Scripts/FinalCase.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FinalCase : Objectif {
+
+	public int objectifIterator;
+	public int totalIterator;
+
+
+	// Use this for initialization
+	void Start () {
+		ArrayList pickingObjfList = new ArrayList ();
+		pickingObjfList = this.GetComponentInParent (PickingObjectif);
+		totalIterator = pickingObjfList.Count ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public override EffectTransformation Effect () {
+		EffectTransformation effect = new EffectTransformation ();
+		effect.isObjectif = true;
+		if (objectifIterator == totalIterator) {
+			effect.isWinner = true;
+		}
+		return effect;
+	}
+
+}
