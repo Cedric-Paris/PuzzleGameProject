7b0ffd89985d761f7bb27213c18a61cc3f97d8a1
Cedric-Paris <Cedric.Paris@etu.udamail.fr>
2015-11-30 23:04:26 +0100

    Fix Player movement issues with x and y < 0
    Le problème est corrigé j'avais pas pris en compte ce cas pour le joueur
    mais je l'avais fait pour les DraggableElement normalement.
    /!\ Mettez bien les éléments au centre des cases sinon avec pas de
    chance l'élément peut être pris en compte par le joueur une case avant.

--- Assets/Scripts/PlayerMovementController.cs
+++ Assets/Scripts/PlayerMovementController.cs
@@ -2,30 +2,38 @@
 using System.Collections;
 
 public class PlayerMovementController : MonoBehaviour {
-
+	
 	public static readonly DirectionProperties GO_UP = new DirectionProperties (new Vector3 (0, 1, 0), new Vector3 (0, 0.5f, 0), 3,
-	                                                                            (currentPos) => { return new Vector3 (currentPos.x, ((int)currentPos.y) + 0.5f, 0);},
+	                                                                            (currentPos) => { 
+																									if(currentPos.y < 0) return new Vector3 (currentPos.x, ((int)currentPos.y) - 0.5f, 0);
+																									return new Vector3 (currentPos.x, ((int)currentPos.y) + 0.5f, 0);},
 																				(currentPos) => {
 																									if (currentPos.y < 0 && (currentPos.y * -1) % 1 <= 0.5f) return true;
 																									if (currentPos.y > 0 && (currentPos.y % 1) >= 0.5f) return true;
 																									return false;
 																								});
 	public static readonly DirectionProperties GO_DOWN = new DirectionProperties(new Vector3(0, -1, 0), new Vector3 (0, -0.5f, 0), 4,
-	                                                                            (currentPos) => { return new Vector3 (currentPos.x, ((int)currentPos.y) + 0.5f, 0);},
+	                                                                            (currentPos) => {
+																									if(currentPos.y < 0) return new Vector3 (currentPos.x, ((int)currentPos.y) - 0.5f, 0);
+																									return new Vector3 (currentPos.x, ((int)currentPos.y) + 0.5f, 0);},
 																				(currentPos) => {
 																									if (currentPos.y < 0 && (currentPos.y * -1) % 1 >= 0.5f) return true;
 																									if (currentPos.y > 0 && (currentPos.y % 1) <= 0.5f) return true;
 																									return false;
 																								});
 	public static readonly DirectionProperties GO_RIGHT = new DirectionProperties(new Vector3(1, 0, 0), new Vector3 (0.5f, 0, 0), 1,
-	                                                                              	(currentPos) => { return new Vector3(((int)currentPos.x)+0.5f, currentPos.y,0);},
+	                                                                              	(currentPos) => {
+																										if(currentPos.x < 0) return new Vector3(((int)currentPos.x)-0.5f, currentPos.y,0);
+																										return new Vector3(((int)currentPos.x)+0.5f, currentPos.y,0);},
 																					(currentPos) => {
 																										if (currentPos.x < 0 && (currentPos.x * -1) % 1 <= 0.5f) return true;
 																										if (currentPos.x > 0 && (currentPos.x % 1) >= 0.5f) return true;
 																										return false;
 																									});
 	public static readonly DirectionProperties GO_LEFT = new DirectionProperties(new Vector3(-1, 0, 0), new Vector3 (-0.5f, 0, 0), 2,
-                                                                            	(currentPos) => { return new Vector3(((int)currentPos.x)+0.5f, currentPos.y,0);},
+                                                                            	(currentPos) => {
+																									if(currentPos.x < 0) return new Vector3(((int)currentPos.x)-0.5f, currentPos.y,0);
+																									return new Vector3(((int)currentPos.x)+0.5f, currentPos.y,0);},
 																				(currentPos) => {
 																									if (currentPos.x < 0 && (currentPos.x * -1) % 1 >= 0.5f) return true;
 																									if (currentPos.x > 0 && (currentPos.x % 1) <= 0.5f) return true;
