da84b2c8b76f5ceeb2c1ab56c9c463cb9d262014
Cedric-Paris <Cedric.Paris@etu.udamail.fr>
2015-12-12 15:47:14 +0100

    Add MapEditor
    - La scène pour l'édition de Map est créée
    - Le menu d'édition est fait et fonctionne avec tout les objets
    normalement (pas seulement les Elements)
    - le menu d'édition est fonctionnel sur portable
    - Il manque la grille est le fait de poser les blocs sur le terrain

---
+++ Assets/Scripts/EditorMenu.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EditorMenu : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		Vector3 r = Camera.main.ScreenToWorldPoint (new Vector3 (Screen.width, Screen.height));
+		Debug.LogError (r.x);
+	}
+}
---
+++ Assets/Scripts/SelectElementOnEditorMenu.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class SelectElementOnEditorMenu : MonoBehaviour {
+
+	public static GameObject selectedObject;
+	public static Button currentButton;
+
+	public void OnButtonPressOnMenuEditor(Button buttonOnMenu)
+	{
+		ElementOnMapEditorMenu e = buttonOnMenu.GetComponentInChildren<ElementOnMapEditorMenu> ();
+		selectedObject = e.gObject;
+		if (currentButton != null)
+			currentButton.image.color = new Color(255,255,255,0);
+		buttonOnMenu.image.color = new Color(255,255,255,255);
+		currentButton = buttonOnMenu;
+	}
+}
