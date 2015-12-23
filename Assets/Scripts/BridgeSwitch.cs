c42a2e21b9e2596a82b7086688006875f9a81c7b
Nawhal <nawhal.sayarh@etu.udamail.fr>
2015-12-23 20:58:41 +0100

    Ajout des sprites d'eau et d'arbre (version améliorée)
    Modification du SwitchBridge afin que l'animation marche et que tous les
    ponts ainsi que tous les autres switchs s'inversent quand on passe sur
    un switch. J'ai donc ajouté une liste de switchs dans SwitchBridge tout
    comme il y a une liste de ponts.
    (Désolée pour les 3 bidouilles d'avant, c'était pour pas avoir à faire
    un Merge Branch)

--- Assets/Scripts/BridgeSwitch.cs
+++ Assets/Scripts/BridgeSwitch.cs
@@ -2,20 +2,31 @@
 using System.Collections;
 
 public class BridgeSwitch : Special {
-	
+
+	private Animator animator;
+	public ArrayList lBridge = new ArrayList();
+	public ArrayList lSwitch = new ArrayList();
 
 	// Use this for initialization
 	void Start () {
-
+		animator = GetComponent<Animator> ();
 	}
 
-	public ArrayList lBridge = new ArrayList();
 
 	public override EffectTransformation Effect (bool isTreated = false) {
-
+		if (isTreated == false)
+			return new EffectTransformation(false);
 		Debug.Log ("Changement de position des ponts");
 		foreach (Bridge i in lBridge)
 			i.ChangePosition ();
+		ChangeColor ();
+		foreach (BridgeSwitch i in lSwitch)
+			i.ChangeColor ();
 		return new EffectTransformation(false);
+	}
+
+	public void ChangeColor()
+	{
+		animator.SetBool ("Active", !animator.GetBool ("Active"));
 	}
 }
