824fce73d1e75ead8aa8aad51ac9a7a3d52eb763
Cedric-Paris <Cedric.Paris@etu.udamail.fr>
2016-01-05 22:49:48 +0100

    Convert Water to Element
    - L'eau est maintenant un élément détectable par le joueur
    - /!\ a l'heure actuelle le joueur passe au dessus de l'eau -> après on
    aura une animation "plouf"
    - modification des levels en conséquence
    - isWater existe maintenant dans EffectTransformation pour gérer les
    actions associées à l'eau

--- Assets/Scripts/Bridge.cs
+++ Assets/Scripts/Bridge.cs
@@ -21,8 +21,7 @@ public class Bridge : Element {
 	public override EffectTransformation Effect (bool isTreated = false) {
 		EffectTransformation effect = new EffectTransformation();
 		if (!this.isDown) {
-			Debug.Log ("Ceci est un obstacle, vous ne pouvez avancer.");
-			effect.isObstacle = true;
+			effect.isWater = true;
 			return effect;
 		}
 		return new EffectTransformation(false);
---
+++ Assets/Scripts/Water.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Water : Element {
+
+	public override EffectTransformation Effect (bool isTreated = false) {
+		EffectTransformation effect = new EffectTransformation ();
+		effect.isWater = true;
+		return effect;
+	}
+}
