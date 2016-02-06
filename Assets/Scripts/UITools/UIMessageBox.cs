c86ecd93683ebd7f9b82681bd4d4389cf881ed5e
Cedric-Paris <Cedric.Paris@etu.udamail.fr>
2016-02-06 18:03:34 +0100

    New architecture for project folders
    - Certains Prefabs sont maintenant dans un dossier Resources.
    - Tous les Prefabs de ce dossier sont susceptibles d'être chargés
    dynamiquement lorsque l'application tourne. -> Resources.load("Prefab")
    
    /!\ Ne pas mettre de Prefabs dans ce dossier si ce n'est pas le cas
    sinon le fichier APK sera surchargé pour rien.

---
+++ Assets/Scripts/UITools/UIMessageBox.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using UnityEditor;
+
+public class UIMessageBox : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		GameObject myButtonPrefab = (GameObject)Resources.Load<GameObject>("BasicSquare");
+		GameObject actualButton = GameObject.Instantiate(myButtonPrefab) as GameObject;
+		GameObject g = new GameObject();
+		Canvas canvas = g.AddComponent<Canvas>();
+		g.AddComponent<CanvasScaler>();
+		g.AddComponent<GraphicRaycaster>();
+		canvas.renderMode = RenderMode.ScreenSpaceCamera;
+		canvas.worldCamera = Camera.main;
+		canvas.sortingOrder = 51;
+
+		GameObject text = new GameObject();
+		text.transform.SetParent(g.transform);
+		Text t = text.AddComponent<Text>();
+		t.font = new Font("Qlassik.ttf");
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+	}
+}
