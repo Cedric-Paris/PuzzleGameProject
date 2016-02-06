ec8915ec2f6c654fa833ac8499e1bdf65f433dff
Cedric-Paris <Cedric.Paris@etu.udamail.fr>
2016-02-06 15:15:33 +0100

    Left Side Panel on Editor Menu
    - Ajout d'un panneau latéral qui peut être ouvert ou fermé par
    l'utilisateur
    Dans ce panneau il aura la possibilité de savegarder, de recharger une
    carte, de vider la map actuelle (retour a zero) et de quitter l'éditeur
    de map

---
+++ Assets/Scripts/MapEditor/SidePanelManager.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SidePanelManager : MonoBehaviour {
+
+	private Animator anim;
+
+	void Start()
+	{
+		anim = GetComponent<Animator>();
+		if(anim == null)
+			Debug.LogError("Error - no animator");
+	}
+
+	public void SetPanelState(bool value)
+	{
+		anim.SetBool("IsOpen",value);
+	}
+}
