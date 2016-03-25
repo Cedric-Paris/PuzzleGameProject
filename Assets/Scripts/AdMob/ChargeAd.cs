bce3432137309ce3ed3ccdd2d8df33a360c3163e
Cedric-Paris <Cedric.Paris@etu.udamail.fr>
2016-03-25 18:06:09 +0100

    AdMob
    - AdMob fonctionnel
    - Actuellement en mode développeur pour mon portable donc ça fonctionne
    qu'avec mon portable mais on peut le passer en mode "réel" et ça marche
    direct aussi
    - Nawhal faut reprendre les couleurs du menu des levels

---
+++ Assets/Scripts/AdMob/ChargeAd.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+using GoogleMobileAds.Api;
+
+public class ChargeAd : MonoBehaviour {
+
+	InterstitialAd i;
+
+	// Use this for initialization
+	void Start () {
+		AdManager.LoadInterstistialAd();
+	}
+	
+	public void Clic () {
+		AdManager.ShowInterstitialAd();
+	}
+
+}
